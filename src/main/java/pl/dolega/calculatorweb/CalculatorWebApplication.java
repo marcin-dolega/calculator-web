0715ff98ffad17f07dd3cd25b7fba1c7bf16df15
Marcin <marcin92d@gmail.com>
2023-02-02 12:23:40 +0100

    Initial commit

---
+++ src/main/java/pl/dolega/calculatorweb/CalculatorWebApplication.java
@@ -0,0 +1,13 @@
+package pl.dolega.calculatorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dolega/calculatorweb/CalculatorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dolega.calculatorweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
